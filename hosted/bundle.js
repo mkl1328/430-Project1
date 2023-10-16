61bc675d1c388a2f8a3b92bbcd186b562fe41a8b
Max Lama <mkl1328@rit.edu>
2023-10-16 07:33:07 -0400

    webpack config setup (i forgot)

---
+++ hosted/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./client/client.js":
+/*!**************************!*\
+  !*** ./client/client.js ***!
+  \**************************/
+/***/ (() => {
+
+eval("const handleResponse = async (response) => {\r\n  //Do the do\r\n}\r\n\r\nconst sendFetch = async (url) => {\r\n  const response = await fetch(url);\r\n  handleResponse(response);\r\n};\r\n\r\nconst init = () => {\r\n  // Set all buttons to send fetch requests\r\n}\r\n\r\nwindow.onload = init;\n\n//# sourceURL=webpack://430-project1/./client/client.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./client/client.js"]();
+/******/ 	
+/******/ })()
+;
